9dc1515232b222cb12a07dea7f0ab52865488b80
¨Clara <carboniclara012ing@gmail.com>
2024-07-24 16:32:21 -0300

    ultima modificacion

--- src/pages/Contacto.js
+++ src/pages/Contacto.js
@@ -39,7 +39,7 @@ const Contacto = (props) => {
                 
             </ul>
             <div className="mapa">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3284.2408793320155!2d-58.42258492536941!3d-34.598069957198014!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcca7d27fdff63%3A0x843209e824224cd3!2sAv.%20Medrano%20991%2C%20C1179%20Cdad.%20Aut%C3%B3noma%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1721670728820!5m2!1ses-419!2sar" title="mapa" ></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3284.2408793320155!2d-58.42258492536941!3d-34.598069957198014!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcca7d27fdff63%3A0x843209e824224cd3!2sAv.%20Medrano%20991%2C%20C1179%20Cdad.%20Aut%C3%B3noma%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1721670728820!5m2!1ses-419!2sar" style={{width:600, height:300, border:0}} title="mapa" ></iframe>
             </div>
         </div>
     </main>
